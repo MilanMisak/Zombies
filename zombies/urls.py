4ef1ea22754f33aea668e99d074aa0b01e96640b
Milan Misak <misakmilan@gmail.com>
2012-05-25 13:40:53 +0100

    Added the game app

--- zombies/urls.py
+++ zombies/urls.py
@@ -5,9 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'zombies.views.home', name='home'),
-    # url(r'^zombies/', include('zombies.foo.urls')),
+    url(r'^$', include('game.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
