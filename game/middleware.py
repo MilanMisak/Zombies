bafd818f36b74b85f87c244c2fd65ef950540b9c
Milan Misak <misakmilan@gmail.com>
2012-06-05 16:46:56 +0100

    Snail on the index page, layout definition clean up

--- game/middleware.py
+++ game/middleware.py
@@ -1,6 +1,10 @@
 from game.models import *
 
 class CleanUpMiddleware(object):
+    """
+    Deletes old games and players.
+    """
+
     def process_request(self, request):
         Game.clean_up()
         Player.clean_up()
