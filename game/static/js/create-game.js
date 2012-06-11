9987bf7adf1c84c7ddcb23f39837ad78bed690f3
Milan Misak <misakmilan@gmail.com>
2012-06-11 14:43:59 +0100

    Bigger AJAX error allowances for create-game and join-game. The whole game should be a lot more stable now

--- game/static/js/create-game.js
+++ game/static/js/create-game.js
@@ -1,18 +1,30 @@
-// Outside the document ready handler for immediate execution
+var AJAX_ERROR_ALLOWANCE = 10; // Keep in sync with models.py setting
+var ajaxErrorCount = 0;
+
 var updateGameInfo = function() {
     $.getJSON('/ajax-game-info', function(data) {
-        var items = [];
+        ajaxErrorCount = 0;
 
+        var items = [];
         $.each(data[2], function(key, val) {
             items.push('<li>' + val + '</li>');
         });
 
         $('#player_list').html('<ul>' + items.join('') + '</ul>');
     }).error(function(xhr, status, data) {
-        $('#error_modal').modal('show');
+        ajaxErrorCount++;
+        if (ajaxErrorCount < AJAX_ERROR_ALLOWANCE)
+            return;
+        ajaxErrorCount = 0;
+
+        if (errorModalShown)
+            return;
+        errorModalShown = true;
+
         $('#error_modal').on('hide', function() {
             window.location.replace('/');
         });
+        $('#error_modal').modal('show');
     });
 };
 
--- game/static/js/join-game.js
+++ game/static/js/join-game.js
@@ -1,9 +1,14 @@
 var documentReady = false;
 var hasJoinedGameYet = false;
 
+var AJAX_ERROR_ALLOWANCE = 20; // Keep in sync with models.py setting, DOUBLED HERE
+var ajaxErrorCount = 0;
+
 // Outside the document ready handler for immediate execution
 var updateGameInfo = function() {
     $.getJSON('/ajax-game-info', function(data) {
+        ajaxErrorCount = 0;
+
         if (data[1] === 1) {
             // Game has started
             window.location.replace('/game');
@@ -21,6 +26,11 @@ var updateGameInfo = function() {
         $('#joined_game_instructions').fadeIn();
         $('#player_list').html('<ul>' + items.join('') + '</ul>');
     }).error(function(xhr, status, data) {
+        ajaxErrorCount++;
+        if (ajaxErrorCount < AJAX_ERROR_ALLOWANCE)
+            return;
+        ajaxErrorCount = 0;
+
         if ('NO-GAME' === xhr.responseText && hasJoinedGameYet) {
             // No game associated with this player anymore
             hasJoinedGameYet = false;
@@ -40,8 +50,9 @@ $(document).ready(function() {
 
     var updateGameList = function() {
         $.getJSON('/ajax-games', function(data) {
-            var items = [];
+            ajaxErrorCount = 0;
 
+            var items = [];
             $.each(data, function(key, val) {
                 items.push('<li>' +
                 '<span>' + val + '</span>' +
@@ -62,6 +73,11 @@ $(document).ready(function() {
                 });
             });
         }).error(function(xhr, status, data) {
+            ajaxErrorCount++;
+            if (ajaxErrorCount < AJAX_ERROR_ALLOWANCE)
+                return;
+            ajaxErrorCount = 0;
+
             alert('An error occurred');
             window.location.replace('/');
         });
