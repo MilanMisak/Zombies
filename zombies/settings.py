345baf95adcc08b98b031dcbbffa53a393cad41e
Milan Misak <misakmilan@gmail.com>
2012-06-12 15:38:40 +0100

    Installed south

--- zombies/settings.py
+++ zombies/settings.py
@@ -122,6 +122,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'game',
+
+    'south',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
