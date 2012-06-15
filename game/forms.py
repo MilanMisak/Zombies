09b00beba3f4c01707a1350e4a0e2084f865ab46
Milan Misak <misakmilan@gmail.com>
2012-06-15 19:21:40 +0100

    Shorter usernames

--- game/forms.py
+++ game/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class LoginForm(forms.Form):
     player_name = forms.CharField(
-        max_length=50,
+        max_length=20,
         label='Player Name:',
         error_messages={'required': 'Yo dawg, tell me your name'})
     create_game = forms.BooleanField(
